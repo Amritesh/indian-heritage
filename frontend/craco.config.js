22a545855cb2b3925d7335b712f0678f40c1809c
Amritesh <amritesh@ip-192-168-29-254.ap-south-1.compute.internal>
2025-09-18 19:21:06 +0530

    added gh pages

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,4 +1,13 @@
+const webpack = require('webpack');
+
 module.exports = {
+  webpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_API_BASE_URL': JSON.stringify('http://localhost:5000')
+      })
+    ]
+  },
   style: {
     postcss: {
       plugins: [
@@ -7,4 +16,4 @@ module.exports = {
       ],
     },
   },
-}
+};
